c86f17bc45ac4ec007c403f5cef8b48a17aa11f4
StudentClaudiu <bogdanc89@yahoo.de>
2025-08-11 19:37:19 +0300

    minor edit

--- WeatherApp/src/app/environments/environment.ts
+++ WeatherApp/src/app/environments/environment.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyAZdclowmTKES4cGt4WKjI9YOQOuOybGOA',
+  apiKey: 'AIza',
   authDomain: 'weatherapp-7f877.firebaseapp.com',
   projectId: 'weatherapp-7f877',
   storageBucket: 'weatherapp-7f877.firebasestorage.app',
